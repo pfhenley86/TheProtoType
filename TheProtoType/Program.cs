d8624b8d3afdab8948cc6d98ec19bfeac398e1ff
Ford Henley <info@fordswebhelp.com>
2025-11-17 12:05:13 -0500

    Add project files.

---
+++ TheProtoType/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TheProtoType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
