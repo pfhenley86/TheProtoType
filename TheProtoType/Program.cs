df1b76d81b8f0ad9699a37d2c50346174edb9429
“pfhenley86” <fordp86@gmail.com>
2025-11-21 10:14:41 -0500

    completed guessing program

--- TheProtoType/Program.cs
+++ TheProtoType/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            bool numToGuess = false;
+            
+            Random rnd = new Random();
+            int randomNum = rnd.Next(1, 100);
+            
+            Console.WriteLine("Enter a number between 1 and 100");
+
+            while (!numToGuess)
+            {
+                Console.WriteLine("Please enter your guess.");
+                var userInput = Convert.ToInt32(Console.ReadLine());
+                
+                if (userInput > randomNum)
+                {
+                    Console.WriteLine($"Your number is too high. Try again");
+                }
+                else if (userInput < randomNum)
+                {
+                    Console.WriteLine($"Your number is too low. Try again");
+                }
+                else
+                {
+                    Console.WriteLine($"Your number is right. You win!");
+                    Console.WriteLine($"The correct number was {randomNum}");
+                }
+            }
+
+            Console.ReadKey();
+
+
         }
     }
 }
